c2f3a351fe9d75d5f4940a6242251f35fd000d06
Oscar Tran <oscartran007@gmail.com>
2025-09-06 03:12:26 -0700

    Create operate function and global vars as needed Created the operate function which calls one of the other operations given two numbers and the operator in question. Initialized some values like the input list and the stored current and previous value.

--- javascript.js
+++ javascript.js
@@ -1,3 +1,7 @@
+const inputs = []
+let previousVal
+let currentVal
+
 const add = function(num1, num2) {
 	return num1 + num2
 };
@@ -12,4 +16,18 @@ const multiply = function(num1, num2){
 
 const divide = function(num1, num2){
     return num1 / num2
+}
+
+const operate = function(num1, num2, operator){
+    switch(operator){
+        case "+":
+            return add(num1, num2)
+        case "-":
+            return subtract(num1, num2)
+        case "*":
+            return multiply(num1, num2)
+        case "/":
+            divide(num1, num2)
+            return
+    }
 }
